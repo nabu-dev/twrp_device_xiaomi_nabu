d28a3d6210449db94a773a7a1ced60863d031054
Rafael Méndez Pacheco <rafy.pachecomendez@gmail.com>
2022-09-01 17:59:43 -0400

    some lot of things for booting twrp with kernel source

---
+++ recovery/root/system/bin/unified-script.sh
@@ -0,0 +1,32 @@
+#!/system/bin/sh
+# set a couple of props, depending on device
+
+load_nabu()
+{
+    resetprop "ro.product.name" "nabu"
+    resetprop "ro.product.model" "Xiaomi Pad 5 (21051182G)"
+}
+
+load_nabucn()
+{
+    resetprop "ro.product.name" "bhima"
+    resetprop "ro.product.model" "Xiaomi Pad 5 China (21051182C)"
+}
+
+variant=$(getprop ro.boot.hwc)
+echo $variant
+
+case $variant in
+    "GLOBAL")
+        load_nabu;
+        ;;
+    "INDIA")
+        load_nabucn;
+        ;;
+    *)
+        load_nabu;
+        ;;
+esac
+
+exit 0;
+
